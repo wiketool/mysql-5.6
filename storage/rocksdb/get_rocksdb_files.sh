d8729d7cf451779cb2e3545be46810a08b245b5f
wiketool <64351271+wiketool@users.noreply.github.com>
2025-07-12 21:08:36 +0800

    fix: compile error for storage/rocksdb/get_rocksdb_files.sh

--- storage/rocksdb/get_rocksdb_files.sh
+++ storage/rocksdb/get_rocksdb_files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 MKFILE=`mktemp`
 # create and run a simple makefile
 # include rocksdb make file relative to the path of this script
@@ -27,7 +28,7 @@ then
 sed -e s/@GIT_SHA@/$git_sha/ -e s:@GIT_TAG@:"$git_tag":  \
     -e s/@GIT_MOD@/"$git_mod"/ -e s/@BUILD_DATE@/"$build_date"/  \
     -e s/@GIT_DATE@/"$git_date"/ \
-    -e s/@ROCKSDB_PLUGIN_BUILTINS@/"$(ROCKSDB_PLUGIN_BUILTINS)"/ \
-    -e s/@ROCKSDB_PLUGIN_EXTERNS@/"$(ROCKSDB_PLUGIN_EXTERNS)"/ \
+    -e s/@ROCKSDB_PLUGIN_BUILTINS@/"$ROCKSDB_PLUGIN_BUILTINS"/ \
+    -e s/@ROCKSDB_PLUGIN_EXTERNS@/"$ROCKSDB_PLUGIN_EXTERNS"/ \
     rocksdb/util/build_version.cc.in > $bv
 fi
